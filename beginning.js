2729b984c8944b360686cd56ea8157e161a9a6ea
Md Shazad <ohan1258@gmail.com>
2021-02-28 17:10:49 +0530

    serving an html file

--- beginning.js
+++ beginning.js
@@ -5,7 +5,10 @@
 //test comment 2
 
 var http=require('http');
+var fs=require('fs');
 http.createServer(function(req,res){
+	fs.readFile('demo.html',function(err,data){
 	res.writeHead(200,{'Content-Type':'text/html'});
-	res.end('Hello World');
+	res.write(data)
+	return res.end();})
 }).listen(8080);
