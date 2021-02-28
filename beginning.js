8b1cc01d061930e3f0ff49e45a129474d8786eee
Md Shazad <ohan1258@gmail.com>
2021-02-28 17:35:29 +0530

    taking port from cli

--- beginning.js
+++ beginning.js
@@ -6,9 +6,10 @@
 
 var http=require('http');
 var fs=require('fs');
+var port=process.argv[2] || 8080;
 http.createServer(function(req,res){
 	fs.readFile('demo.html',function(err,data){
 	res.writeHead(200,{'Content-Type':'text/html'});
 	res.write(data)
 	return res.end();})
-}).listen(8080);
+}).listen(parseInt(port));
